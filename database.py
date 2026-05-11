842c36d1fc26b8f7d627be99c5e64e09f46a3354
kds890808 <deoksu.kim88@gmail.com>
2026-05-11 15:42:44 +0900

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./db.sqlite3"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
