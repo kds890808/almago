0fa52c1ca9dc379ce551dd6103fae3c1149056c8
kds890808 <deoksu.kim88@gmail.com>
2026-05-11 16:23:09 +0900

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,24 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./db.sqlite3"
-
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./db.sqlite3"
 )
-SessionLocal = sessionmaker(bind=engine)
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
