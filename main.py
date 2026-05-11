ddb9ee10f700c9159b85954c08afb2fd5ecacc9a
kds890808 <deoksu.kim88@gmail.com>
2026-05-11 13:55:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -63,9 +63,11 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "https://gentle-pastelito-28f314.netlify.app"
+        "https://gentle-pastelito-28f314.netlify.app",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500"
     ],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
